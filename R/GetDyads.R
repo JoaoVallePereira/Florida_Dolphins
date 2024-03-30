617226d431401c3945b981ff13d09be9233e2503
do Valle Pereira <dovallej@oregonstate.edu>
2024-03-30 16:08:10 -0700

    Context networks

--- R/GetDyads.R
+++ R/GetDyads.R
@@ -25,22 +25,112 @@ data.raw <- read.csv("./data/raw/data_for_SOCPROG.csv") %>%
   tidyr::drop_na(IDs)
 head(data.raw)
 
-# Get GBI ----
+dfINFO <- data.raw %>% 
+  dplyr::select(date, foraging, tactic, zone, IDs) 
+
+# Long data frame with groups column
+
+dfINFO_long <- dfINFO %>% 
+  dplyr::mutate(group = rownames(.)) %>% 
+  tidyr::separate_rows(IDs, sep = " ") %>%
+  data.frame()
+dfGroupSize <- dfINFO_long %>% 
+  dplyr::group_by(group) %>% 
+  dplyr::count()
+
+dfINFO_long <- dfINFO_long %>% 
+  dplyr::left_join(dfGroupSize, by = "group") %>% 
+  dplyr::filter(n > 1)
+  
+head(dfINFO_long)
+
+# Filter IDs > 1
+dfCount <- dfINFO_long %>% 
+  dplyr::group_by(IDs) %>% 
+  dplyr::count() %>% 
+  dplyr::filter(n > 1)
+dfIDs <- dfCount$IDs
+
+dfINFO_long_filter <- dfINFO_long %>% 
+  dplyr::filter(IDs %in% dfIDs)
+
+dfIdentities <- dfINFO_long_filter %>% 
+  dplyr::group_by(group) %>% 
+  dplyr::summarise(identities = paste(IDs, collapse=" ")) %>% 
+  dplyr::ungroup()
+
+###############################################
+# Get GBI all ----
 
 dataForGBI <- data.raw %>% 
   dplyr::group_by(date, daily_sighting) %>%
   dplyr::mutate(group_size = sum(lengths(identities))) %>% 
-  dplyr::filter(group_size > 1) %>% 
+  # dplyr::filter(group_size > 1) %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(obs_id = rownames(.))
 
 rawGBI <- asnipe::get_group_by_individual(dataForGBI$identities, 
-                                          data_format = "groups")
+                                          data_format = "groups")  
+# rawGBI <- rawGBI[,colSums(rawGBI) > 1]  
+obsALL <- rawGBI
 
-obs <- rawGBI
+
+# Get GBI no forag----
+
+dataForGBI_Nofor <- data.raw %>% 
+  dplyr::filter(foraging == "No") %>% 
+  dplyr::group_by(date, daily_sighting) %>%
+  dplyr::mutate(group_size = sum(lengths(identities))) %>% 
+  # dplyr::filter(group_size > 1) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(obs_id = rownames(.))
+
+Nofor_GBI <- asnipe::get_group_by_individual(dataForGBI_Nofor$identities, 
+                                          data_format = "groups")
+# Nofor_GBI <- Nofor_GBI[,colSums(Nofor_GBI) > 1]  
+obsNoFor <- Nofor_GBI
+
+
+# Get GBI forag----
+
+dataForGBI_For <- data.raw %>% 
+  dplyr::filter(foraging == "Yes" |
+                foraging == "Prob") %>% 
+  dplyr::group_by(date, daily_sighting) %>%
+  dplyr::mutate(group_size = sum(lengths(identities))) %>% 
+  # dplyr::filter(group_size > 1) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(obs_id = rownames(.))
+
+For_GBI <- asnipe::get_group_by_individual(dataForGBI_For$identities, 
+                                             data_format = "groups")
+# For_GBI <- For_GBI[,colSums(For_GBI) > 1]  
+obsFor <- For_GBI
+
+# Get GBI MRF----
+
+dataForGBI_MRF <- data.raw %>% 
+  dplyr::filter(tactic == "MRF") %>% 
+  dplyr::group_by(date, daily_sighting) %>%
+  dplyr::mutate(group_size = sum(lengths(identities))) %>% 
+  # dplyr::filter(group_size > 1) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::mutate(obs_id = rownames(.))
+
+MRF_GBI <- asnipe::get_group_by_individual(dataForGBI_MRF$identities, 
+                                           data_format = "groups")
+# MRF_GBI <- MRF_GBI[,colSums(MRF_GBI) > 1]  
+obsMRF <- MRF_GBI
+
+
 
 ## Convert to long format (dyad) TAKES SOME TIME (load rds in the sequence) !!!! ----
 df <- data.frame(node_1=numeric(), node_2=numeric(), social_event=numeric(), obs_id=numeric())
+# obs <- obsALL
+obs   <- obsNoFor
+# obs <- obsFor
+# obs <- obsMRF
+
 for (obs_id in 1:nrow(obs)) {
   for (i in which(obs[obs_id, ] == 1)) {
     for (j in 1:ncol(obs)) {
@@ -61,12 +151,23 @@ for (obs_id in 1:nrow(obs)) {
 head(df)
 
 # Behavior, tactic and zone status ----
-rawINFO <- dataForGBI %>% 
+rawINFO_Nofor <- dataForGBI_Nofor %>% 
   dplyr::select(date, foraging, tactic, zone, obs_id) %>% 
   dplyr::mutate(obs_id = as.numeric(obs_id))
 
+# Behavior, tactic and zone status ----
+rawINFO_For <- dataForGBI_For %>% 
+  dplyr::select(date, foraging, tactic, zone, obs_id) %>% 
+  dplyr::mutate(obs_id = as.numeric(obs_id))
+
+# Behavior, tactic and zone status ----
+rawINFO_MRF <- dataForGBI_MRF %>% 
+  dplyr::select(date, foraging, tactic, zone, obs_id) %>% 
+  dplyr::mutate(obs_id = as.numeric(obs_id))
+
+
 # Get dyads with observations information
-dfDyads_full2 <- df %>% 
+dfDyads_full <- df %>% 
   dplyr::left_join(rawINFO, by = "obs_id") %>% 
   dplyr::mutate(node_1 = as.factor(node_1),
                 node_2 = as.factor(node_2)) %>%
@@ -76,6 +177,34 @@ dfDyads_full2 <- df %>%
 # saveRDS(df4, file = "./data/processed/DF_dyads.rds")
 
 
+# Get dyads with observations information
+dfDyads_Nofor <- df %>% 
+  dplyr::left_join(rawINFO_Nofor, by = "obs_id") %>% 
+  dplyr::mutate(node_1 = as.factor(node_1),
+                node_2 = as.factor(node_2)) %>%
+  dplyr::group_by(node_1, node_2) %>%
+  dplyr::mutate(dyad_id = dplyr::cur_group_id()) %>%
+  dplyr::mutate(obs_id = as.integer(obs_id))
+  # saveRDS(dfDyads_Nofor, file = "./data/processed/DF_dyadsNofor.rds")
 
 
+# Get dyads with observations information
+dfDyads_For <- df %>% 
+  dplyr::left_join(rawINFO_For, by = "obs_id") %>% 
+  dplyr::mutate(node_1 = as.factor(node_1),
+                node_2 = as.factor(node_2)) %>%
+  dplyr::group_by(node_1, node_2) %>%
+  dplyr::mutate(dyad_id = dplyr::cur_group_id()) %>%
+  dplyr::mutate(obs_id = as.integer(obs_id))
+ # saveRDS(dfDyads_For, file = "./data/processed/DF_dyadsFor.rds")
+
+# Get dyads with observations information
+dfDyads_MRF <- df %>% 
+  dplyr::left_join(rawINFO_MRF, by = "obs_id") %>% 
+  dplyr::mutate(node_1 = as.factor(node_1),
+                node_2 = as.factor(node_2)) %>%
+  dplyr::group_by(node_1, node_2) %>%
+  dplyr::mutate(dyad_id = dplyr::cur_group_id()) %>%
+  dplyr::mutate(obs_id = as.integer(obs_id))
+# saveRDS(dfDyads_MRF, file = "./data/processed/DF_dyadsMRF.rds")
 
---
+++ R/GetNework_originaldata.R
@@ -0,0 +1,97 @@
+########### Network exploration / analysis
+
+# Load dyads data
+
+fit_edge <- readRDS("./data/processed/fit_edge.rds")
+fit_null <- readRDS("./data/processed/fit_null.rds")
+fit_brm <- readRDS("./data/processed/fit_brm.rds")
+
+df_dyads <- readRDS(file = "./data/processed/DF_dyads.rds") %>% 
+  dplyr::mutate(duration = 1)
+head(df_dyads)
+
+# Define priors
+
+priors <- bisonR::get_default_priors("binary")
+priors
+
+bisonR::prior_check(priors, "binary")
+
+# Fit bison model
+fit_edge <- bisonR::bison_model(
+  (social_event | duration) ~ dyad(node_1, node_2), 
+  data = df_dyads, 
+  model_type = "binary",
+  priors = priors)
+
+# Check if MCMC algorithm has behaved correctly
+bisonR::plot_trace(fit_edge, par_ids=2)
+
+# Check the predictions from the fitted model against the real data
+bisonR::plot_predictions(fit_edge, num_draws=20, type="density")
+bisonR::plot_predictions(fit_edge, num_draws=20, type="point")
+summary(fit_edge)
+
+# Plot the network
+bisonR::plot_network(fit_edge, lwd = 5)
+
+
+# Bayesian version of the Bejder et al. 1998 test for non-random association
+fit_null <- bisonR::bison_model(
+  (social_event | duration) ~ 1, 
+  data = df_dyads, 
+  model_type = "binary",
+  priors = priors)
+
+bisonR::model_comparison(list(non_random_model = fit_edge, random_model = fit_null))
+
+
+# NETWORK METRICS ----
+
+# Global ----
+# Coefficient of variation (CV) in edge weights
+cv_samples <- extract_metric(fit_edge, "global_cv")
+head(cv_samples)
+# Visualise the posterior distribution of social differentiation
+plot(density(cv_samples))
+
+
+# Node ----
+# Node eigenvector centrality
+cent_samples <- extract_metric(fit_edge, "node_eigen")
+cent_samples[1:6, 1:5]
+plot(density(cent_samples))
+
+# Node betweenness
+bet_samples <- extract_metric(fit_edge, "node_betweenness")
+bet_samples[1:6, 1:5]
+
+# Regression test
+df_dyadic <- df_dyads %>%
+  dplyr::filter(foraging == "Yes" |
+                  foraging == "Prob") %>% 
+  dplyr::mutate(tactic_mrf = as.factor(ifelse(tactic == "MRF", "MRF", "OTHER"))) %>% 
+  dplyr::distinct(node_1, node_2, tactic_mrf)
+df_dyadic
+
+fit_dyadic <- bisonR::bison_brm(
+  bison(edge_weight(node_1, node_2)) ~ tactic_mrf,
+  fit_edge,
+  df_dyadic,
+  num_draws = 5, # Small sample size for demonstration purposes
+  refresh = 0)
+summary(fit_dyadic)
+
+
+
+fit_dyads_tactic <- bisonR::bison_brm(
+  bison(edge_weight(node_1, node_2)) ~ tactic,
+  fit_edge,
+  df_dyads,
+  num_draws = 5, # Small sample size for demonstration purposes
+  refresh = 0)
+summary(fit_dyadic)
+
+saveRDS(fit_edge, file = "./data/processed/fit_edge.rds")
+saveRDS(fit_dyads_tactic, file = "./data/processed/fit_brm.rds")
+saveRDS(fit_null, file = "./data/processed/fit_null.rds")
